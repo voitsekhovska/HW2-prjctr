3d26b93fbe8e607b8bc044e097a6810c222d7f83
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-01-21 00:23:26 +0200

    homework

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// 1. Перетворення типів
+
+let number = 8;
+let string = number.toString();
+console.log(string);
+
+let bookChapter = "8";
+let amount = parseInt(bookChapter);
+console.log(amount);
+
+let quantity = 2;
+let isActive = Boolean(quantity);
+console.log(isActive);
+
+// 2. Конвертер валют
+
+let rate = 37.42;
+let total = +prompt("Введіть суму в гривнях: ");
+console.log(Math.floor(total * rate));
+
+// 3. Обчислення вартості бензину
+
+let petrolAmount = +prompt("Потрібно літрів бензину: ");
+let price = +prompt("Ціна бензину за літр: ");
+let overallPrice = petrolAmount * price;
+console.log(overallPrice.toFixed(2));
